296658619a925133a91540d59b3429c6c86057c0
laokashouji <fantesticday@gmail.com>
2023-03-19 22:26:35 +0800

    #12 修改codegen

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -115,10 +115,16 @@ llvm::Value *ProcStmtNode::codegen(CodegenContext &context) {
 
 /* -------- const value nodes -------- */
 llvm::Value *StringNode::codegen(CodegenContext &context) { return context.builder.CreateGlobalStringPtr(val); }
-llvm::Value *BoolenNode::codegen(CodegenContext &context) { return context.builder.getInt1(val); }
-llvm::Value *RealNode::codegen(CodegenContext &context) { return ConstantFP::get(llvm_context, APFloat(Val)); }
-llvm::Value *IntegerNode::codegen(CodegenContext &context) { return context.builder.getInt32(val); }
-llvm::Value *CharNode::codegen(CodegenContext &context) { return context.builder.getint8(val); }
+llvm::Value *BoolenNode::codegen(CodegenContext &context) { return val == true ? context.builder.getTrue() : context.builder.getFalse(); }
+llvm::Value *RealNode::codegen(CodegenContext &context) {
+  auto *type = context.builder.getDoubleTy();
+  return llvm::ConstantFP::get(type, val);
+}
+llvm::Value *IntegerNode::codegen(CodegenContext &context) {
+  auto *type = context.builder.getInt32Ty();
+  return llvm::ConstantFP::get(type, val);
+}
+llvm::Value *CharNode::codegen(CodegenContext &context) { return context.builder.getInt8(static_cast<uint8_t>(val)); }
 
 /* -------- decl nodes -------- */
 llvm::Value *ConstListNode::codegen(CodegenContext &context) {
