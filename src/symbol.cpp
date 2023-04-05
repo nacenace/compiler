ed7f2e0cbed5f54afaf97ca05e1b8b3993092747
Laokashouji <fantesticday@gmail.com>
2023-04-05 14:09:19 +0800

    #5 增加symbol.hpp中addGlobalSymbol对arrayType的支持

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -54,10 +54,10 @@ bool SymbolTable::addGlobalSymbol(string name, shared_ptr<TypeNode> type, llvm::
     case llvm::Type::DoubleTyID:
       constant = (initializer == nullptr) ? llvm::ConstantFP::get(llvmtype, 0) : initializer;
       break;
-    /*
     case llvm::Type::ArrayTyID:
-        constant=llvm::ConstantAggregateZero::get(cast_node<ArrayTypeNode>(type)->element_type->get_llvm_type(context));
-        break;
+      constant=llvm::ConstantAggregateZero::get(cast_node<ArrayTypeNode>(type)->elementType.get_llvm_type(context));
+      break;
+        /*
     case llvm::Type::StructTyID:
         constant=llvm::ConstantAggregateZero::get(cast_node<RecordTypeNode>(type)->innertype);
         break;
