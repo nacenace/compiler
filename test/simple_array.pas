6d8f8f635f35d7ca926009c51c6dce024af4b664
Laokashouji <fantesticday@gmail.com>
2023-04-17 02:45:54 +0800

    #29 已可成功跑通该样例

---
+++ test/simple_array.pas
@@ -0,0 +1,12 @@
+program prop;
+var
+  n: array [1..10] of integer;
+
+begin
+    Read(n[1]);
+    Read(n[5]);
+    Read(n[10]);
+    writeln('n[1] = ', n[1]);
+    writeln('n[5] = ', n[5]);
+    writeln('n[10] = ', n[10]);
+end.
