87aacc151ddd636b5f5daa2d78384de02f5a4607
Adam-Teng <soothteng@outlook.com>
2023-05-10 13:26:13 +0800

    add sample error test

---
+++ test/error/error2.pas
@@ -0,0 +1,13 @@
+Program prog;
+
+Var
+x, y : integer;
+a : record
+  title: array [1..50] of char;
+  author: array [1..50] of char;
+  subject: array [1..100] of char;
+  bookid: integer;
+end;
+Begin
+  x := a > y;
+End.
